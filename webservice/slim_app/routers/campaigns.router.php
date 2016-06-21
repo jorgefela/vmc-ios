35055f9c00db5b474ce04d0cff234cfaf0fb5808
Daniel Garcia <daniel2864@gmail.com>
2016-06-21 05:21:05 -0430

    ruta inecesaria

--- webservice/slim_app/routers/campaigns.router.php
+++ webservice/slim_app/routers/campaigns.router.php
@@ -1,7 +1,6 @@
 <?php
 $app->get('/user/:id_user/campaigns(/:id)','authenticate', function ($id_user, $id=0) use ($app) {
 
-
   $app->contentType('application/json');
 
   if(!empty($id_user) && isInteger($id_user) && $id==0){
@@ -24,23 +23,3 @@ $app->get('/user/:id_user/campaigns(/:id)','authenticate', function ($id_user, $
   }
 
 });
-
-/*$app->get('/user/:id_user/campaigns/:id','authenticate', function ($id) use ($app) {
-
-
-
-  $app->contentType('application/json');
-
-  if(!empty($id) && isInteger($id)){
-
-      $ob = new models\Campaigns();
-      $data = $ob->getCampaignsOnly($id);
-      echo '{"reponse": "1", "result": '.json_encode($data).', "message":""}';
-
-  }else{
-
-     $data =array();
-     echo '{"reponse": "1", "result": '.json_encode($data).', "message":"No records found"}';
-      
-  }
-});*/
