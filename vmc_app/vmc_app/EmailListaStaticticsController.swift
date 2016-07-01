90411a7a4bb38a6904041db582077844901c8346
Daniel Garcia <daniel2864@gmail.com>
2016-07-01 17:22:15 -0430

    email list view cell - controller email list

---
+++ vmc_app/vmc_app/EmailListaStaticticsController.swift
@@ -0,0 +1,15 @@
+//
+//  EmailListaStaticticsController.swift
+//  vmc_app
+//
+//  Created by macbook3 on 1/7/16.
+//  Copyright © 2016 HispanoSoluciones. All rights reserved.
+//
+
+import UIKit
+class EmailListStaticticsController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
