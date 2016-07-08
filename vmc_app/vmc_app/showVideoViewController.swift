9b00a42101211cb7c2327a4939421a3eb7754cac
Daniel Garcia <daniel2864@gmail.com>
2016-07-08 18:57:45 -0430

    contains table view

---
+++ vmc_app/vmc_app/showVideoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  showVideoViewController.swift
+//  vmc_app
+//
+//  Created by macbook3 on 8/7/16.
+//  Copyright © 2016 HispanoSoluciones. All rights reserved.
+//
+
+import UIKit
+
+class showVideoViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("cargue controlador que muestra video")
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
