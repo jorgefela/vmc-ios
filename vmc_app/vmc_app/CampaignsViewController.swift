61ef9dcf62c42bac7120f5c4b1c2ac7bb877d5f6
Daniel Garcia <daniel2864@gmail.com>
2016-07-13 22:50:23 -0430

    no message

--- vmc_app/vmc_app/CampaignsViewController.swift
+++ vmc_app/vmc_app/CampaignsViewController.swift
@@ -157,12 +157,21 @@ class CampaignsViewController: UIViewController, UITableViewDataSource, UITableV
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:CustomTableViewCellCampaigns = self.TableViewCampaings.dequeueReusableCellWithIdentifier("miCellCampaing2")! as! CustomTableViewCellCampaigns
         cell.LabelNombreCampaing!.text = ListCampaigns[indexPath.row]
+        /*
+        cell.dataDelivery.text = "0"
+        cell.dataOpens.text = "0"
+        cell.dataPlays.text = "0"
+        cell.dataClick.text = "0"
+        cell.dataBounces.text = "0"
+        cell.dataSpam.text = "0"
+ */
         return cell
     }
     
     // implementacion de metodo delegado
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
         let previaSeleccion = filaSeleccionada
         if indexPath == filaSeleccionada {
             filaSeleccionada = nil
@@ -174,109 +183,123 @@ class CampaignsViewController: UIViewController, UITableViewDataSource, UITableV
         if let anterior = previaSeleccion {
             filas += [anterior]
         }
+        var statusCelda:String = ""
         if let filaPresente = filaSeleccionada {
             filas += [filaPresente]
+            statusCelda = "show"
         }
         
         
         
         //start consulta estadistica
-        PreLoading().showLoading()
-        let myCell = tableView.cellForRowAtIndexPath(indexPath) as! CustomTableViewCellCampaigns
-        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
-        let idUser:Int = prefs.integerForKey("IDUSER") as Int
-        let keyServer:String = (prefs.valueForKey("KEY") as? String)!
-        let fecha_envio = self.ListFechaCampaigns[indexPath.row]
-        let id_cat = self.ListIdCategoriaCampaigns[indexPath.row]
-        let url_path: String = mainInstance.urlBase + "public/user/\(idUser)/statictics/start_date/\(fecha_envio)/cat/\(id_cat)"
-        let url = NSURL(string: url_path)
-        let request: NSMutableURLRequest = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "GET"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.setValue("\(keyServer)", forHTTPHeaderField: "key")
-        
-        //start task
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+        if statusCelda == "show"{
+            PreLoading().showLoading()
+            let myCell = tableView.cellForRowAtIndexPath(indexPath) as! CustomTableViewCellCampaigns
+            let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+            let idUser:Int = prefs.integerForKey("IDUSER") as Int
+            let keyServer:String = (prefs.valueForKey("KEY") as? String)!
+            let fecha_envio = self.ListFechaCampaigns[indexPath.row]
+            let id_cat = self.ListIdCategoriaCampaigns[indexPath.row]
+            let url_path: String = mainInstance.urlBase + "public/user/\(idUser)/statictics/start_date/\(fecha_envio)/cat/\(id_cat)"
+            print(url_path)
+            let url = NSURL(string: url_path)
+            let request: NSMutableURLRequest = NSMutableURLRequest(URL: url!)
+            request.HTTPMethod = "GET"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.setValue("\(keyServer)", forHTTPHeaderField: "key")
             
-            let res = response as! NSHTTPURLResponse!;
-            
-            if (res.statusCode >= 200 && res.statusCode < 300) {
+            //start task
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
                 
-                if error != nil{print(error?.localizedDescription)}
+                let res = response as! NSHTTPURLResponse!;
                 
-                do{
+                if (res.statusCode >= 200 && res.statusCode < 300) {
                     
-                    if let dictionary_result = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
-                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                            
-                            
-                            // start barrido datos
-                            
-                            if let json = dictionary_result["result"] as? NSArray  {
-                                for item in json {
-                                    if let getdelivery = item.valueForKey("delivery") {
-                                        myCell.dataDelivery.text = "\(getdelivery)"
-                                    }else{
-                                        myCell.dataDelivery.text = "0"
+                    if error != nil{print(error?.localizedDescription)}
+                    
+                    do{
+                        
+                        if let dictionary_result = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                
+                                
+                                // start barrido datos
+                                
+                                if let json = dictionary_result["result"] as? NSArray  {
+                                    for item in json {
+                                        if let getdelivery = item.valueForKey("delivery") {
+                                            myCell.dataDelivery.text = "\(getdelivery)"
+                                        }else{
+                                            myCell.dataDelivery.text = "0"
+                                        }
+                                        
+                                        if let getdataOpens = item.valueForKey("opens") {
+                                            myCell.dataOpens.text = "\(getdataOpens)"
+                                        }else{
+                                            myCell.dataOpens.text = "0"
+                                        }
+                                        
+                                        if let getdataPlays = item.valueForKey("plays") {
+                                            myCell.dataPlays.text = "\(getdataPlays)"
+                                        }else{
+                                            myCell.dataPlays.text = "0"
+                                        }
+                                        
+                                        if let getdataClick = item.valueForKey("click") {
+                                            myCell.dataClick.text = "\(getdataClick)"
+                                        }else{
+                                            myCell.dataClick.text = "0"
+                                        }
+                                        
+                                        if let getdataBounces = item.valueForKey("bounces") {
+                                            myCell.dataBounces.text = "\(getdataBounces)"
+                                        }else{
+                                            myCell.dataBounces.text = "0"
+                                        }
+                                        
+                                        if let getdataSpam = item.valueForKey("spam") {
+                                            myCell.dataSpam.text = "\(getdataSpam)"
+                                        }else{
+                                            myCell.dataSpam.text = "0"
+                                        }
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                    }//fin for
+                                    if filas.count > 0 {
+                                        tableView.reloadRowsAtIndexPaths(filas, withRowAnimation: UITableViewRowAnimation.Automatic)
                                     }
-                                    
-                                    if let getdataOpens = item.valueForKey("opens") {
-                                        myCell.dataOpens.text = "\(getdataOpens)"
-                                    }else{
-                                        myCell.dataOpens.text = "0"
-                                    }
-                                    
-                                    if let getdataPlays = item.valueForKey("plays") {
-                                        myCell.dataPlays.text = "\(getdataPlays)"
-                                    }else{
-                                        myCell.dataPlays.text = "0"
-                                    }
-                                    
-                                    if let getdataClick = item.valueForKey("click") {
-                                        myCell.dataClick.text = "\(getdataClick)"
-                                    }else{
-                                        myCell.dataClick.text = "0"
-                                    }
-                                    
-                                    if let getdataBounces = item.valueForKey("bounces") {
-                                        myCell.dataBounces.text = "\(getdataBounces)"
-                                    }else{
-                                        myCell.dataBounces.text = "0"
-                                    }
-                                    
-                                    if let getdataSpam = item.valueForKey("spam") {
-                                        myCell.dataSpam.text = "\(getdataSpam)"
-                                    }else{
-                                        myCell.dataSpam.text = "0"
-                                    }
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
-                                }//fin for
-                                if filas.count > 0 {
-                                    print("aqui entre")
-                                    tableView.reloadRowsAtIndexPaths(filas, withRowAnimation: UITableViewRowAnimation.Automatic)
+                                    //self.TableViewCampaings.reloadData()
+                                    PreLoading().hideLoading()
                                 }
-                                //self.TableViewCampaings.reloadData()
-                                PreLoading().hideLoading()
-                            }
+                                
+                                // end barrido datos
+                                
+                                
+                            })
                             
-                            // end barrido datos
                             
-                            
-                        })
+                        }
+                    }catch{
+                        // si sucede algun problema se imprime el problema en consola
+                        print("ocurrio un error")
+                        print(error)
+                        let appDomain = NSBundle.mainBundle().bundleIdentifier
+                        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
                         
-                        
+                        dispatch_async(dispatch_get_main_queue()){
+                            PreLoading().hideLoading()
+                            self.navigationController!.popToRootViewControllerAnimated(true)
+                            
+                        }
                     }
-                }catch{
-                    // si sucede algun problema se imprime el problema en consola
-                    print("ocurrio un error")
-                    print(error)
+                }else{
+                    NSLog("Response code: %ld", res.statusCode);
                     let appDomain = NSBundle.mainBundle().bundleIdentifier
                     NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
                     
@@ -285,25 +308,17 @@ class CampaignsViewController: UIViewController, UITableViewDataSource, UITableV
                         self.navigationController!.popToRootViewControllerAnimated(true)
                         
                     }
-                }
-            }else{
-                NSLog("Response code: %ld", res.statusCode);
-                let appDomain = NSBundle.mainBundle().bundleIdentifier
-                NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+                }//fin validar response.status
                 
-                dispatch_async(dispatch_get_main_queue()){
-                    PreLoading().hideLoading()
-                    self.navigationController!.popToRootViewControllerAnimated(true)
-                    //self.dismissViewControllerAnimated(true, completion: nil)
-                    //self.performSegueWithIdentifier("panel", sender: self)
-                    
-                }
-            }//fin validar response.status
+            })
             
-        })
+            task.resume()
+            //end task
+        }
+        if filas.count > 0 {
+            tableView.reloadRowsAtIndexPaths(filas, withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
         
-        task.resume()
-        //end task
        
         //end consulta estadistica
         
