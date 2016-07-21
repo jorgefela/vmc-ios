a84fcf3896fadc42d551380d2090790d088f8b6d
Daniel Garcia <daniel2864@gmail.com>
2016-07-21 14:25:58 -0430

    no message

---
+++ vmc_app/vmc_app/SliderMenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SliderMenuViewController.swift
+//  vmc_app
+//
+//  Created by macbook3 on 21/7/16.
+//  Copyright © 2016 HispanoSoluciones. All rights reserved.
+//
+
+import Foundation
+
+class SliderMenuViewController: UITableViewController {
+
+}
