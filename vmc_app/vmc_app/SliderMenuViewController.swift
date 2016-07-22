9a4507e20b17310ae0560d4b1cdba578aa02a0a3
Daniel Garcia <daniel2864@gmail.com>
2016-07-22 19:06:20 -0430

    no message

---
+++ vmc_app/vmc_app/SliderMenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SliderMenuViewController.swift
+//  vmc_app
+//
+//  Created by macbook3 on 22/7/16.
+//  Copyright © 2016 HispanoSoluciones. All rights reserved.
+//
+
+import UIKit
+
+class SliderMenuViewController: UIViewController{
+    
+    @IBOutlet weak var ImagenPerfil: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.ImagenPerfil.layer.cornerRadius = self.ImagenPerfil.frame.size.width / 2
+        self.ImagenPerfil.clipsToBounds = true
+        print("cargue imagen")
+    }
+}
