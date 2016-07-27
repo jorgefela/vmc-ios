3754d121ad0d8bd1af08876cfcc0d8db57c75e30
Daniel Garcia <daniel2864@gmail.com>
2016-07-27 10:30:57 -0430

    no message

--- vmc_app/vmc_app/AddListViewController.swift
+++ vmc_app/vmc_app/AddListViewController.swift
@@ -18,8 +18,27 @@ class AddListViewController: UIViewController {
     
     @IBOutlet weak var TablaElementosListas: UITableView!
     
+    var ElementosListDefault = [
+        "Email Address",
+        "First Name",
+        "Last Name",
+        "Organization Name",
+        "Job Title",
+        "Phone Number",
+        "Street Address",
+        "City",
+        "State/Province",
+        "Zip/Post Code"
+    ]
+    
+    var ElementosListNuevos = [
+        " "
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.TablaElementosListas.registerClass(UITableViewCell.self, forCellReuseIdentifier: "CellAddList")
         //START color navigation controller
         self.navigationController?.setNavigationBarHidden(false, animated: true)
         self.navigationController!.navigationBar.barTintColor = FuncGlobal().UIColorFromRGB(mainInstance.colorCabecera)
@@ -28,6 +47,35 @@ class AddListViewController: UIViewController {
         let colorTxtTitulo: NSDictionary = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         self.navigationController!.navigationBar.titleTextAttributes = colorTxtTitulo as? [String : AnyObject]
         //END color navigation controller
+    }
+    
+    //metodos UITableView
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.ElementosListDefault.count
+        //return self.ElementosListNuevos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.TablaElementosListas.dequeueReusableCellWithIdentifier("CellAddList")!
+
+        
+        cell.textLabel!.text = ElementosListDefault[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+
+        dispatch_async(dispatch_get_main_queue()){
+            //self.id = self.listId[indexPath.row]
+            //self.nombreE = self.ListEmailNombre[indexPath.row]
+            //self.subjectE = self.ListEmailDescripcion[indexPath.row]
+            //self.performSegueWithIdentifier("segue_a_campanias", sender: self)
+            
+        }
+        TablaElementosListas.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
     @IBAction func BtnSegmentoSelccion(sender: UISegmentedControl) {
