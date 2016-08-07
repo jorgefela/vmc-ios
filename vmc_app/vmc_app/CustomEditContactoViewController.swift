1f70a28633151e38807c39280295da744a6b4e7a
Daniel Garcia <daniel2864@gmail.com>
2016-08-06 19:37:48 -0430

    no message

---
+++ vmc_app/vmc_app/CustomEditContactoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CustomEditContactoViewController.swift
+//  vmc_app
+//
+//  Created by macbook3 on 6/8/16.
+//  Copyright © 2016 HispanoSoluciones. All rights reserved.
+//
+
+import UIKit
+
+class CustomEditContactoViewController: UITableViewCell {
+
+    @IBOutlet weak var nombreCampo: UILabel!
+    @IBOutlet weak var FieldContacto: UITextField!
+    
+}
--- vmc_app/vmc_app/EditarContactoViewController.swift
+++ vmc_app/vmc_app/EditarContactoViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-class EditarContactoViewController: UIViewController {
+class EditarContactoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // fila proveniente del detalle 
     // de la lista contacto
@@ -15,9 +15,48 @@ class EditarContactoViewController: UIViewController {
     
     var idList : String = ""
     
+    var LabelArray = ["email", "name", "last name", "phone"]
+    var CampoArray = ["", "", "", ""]
+    
+    @IBOutlet weak var tableViewContacto: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableViewContacto.beginUpdates()
+        //self.tableViewContacto.separatorStyle = UITableViewCellSeparatorStyle.SingleLine
+        self.tableViewContacto.separatorStyle = UITableViewCellSeparatorStyle.None
+        self.tableViewContacto.endUpdates()
         print("\(filaSeleccionada) \(idList)")
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var count = 0
+        count = self.LabelArray.count
+        
+        return count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:CustomEditContactoViewController = self.tableViewContacto.dequeueReusableCellWithIdentifier("cell_edit_contact")! as! CustomEditContactoViewController
+        
+        cell.nombreCampo.text = self.LabelArray[indexPath.row]
+        
+
+        cell.nombreCampo.font = UIFont(name: "HelveticaNeue", size: 16)!
+        cell.nombreCampo.textColor = UIColor.grayColor()
+        let separatorLineView: UIView = UIView(frame: CGRectMake(0, 0, view.frame.size.width, 2))
+        separatorLineView.backgroundColor = UIColor.grayColor()
+        cell.contentView.addSubview(separatorLineView)
+        //separatorLineView.backgroundColor = UIColor(red: 247, green: 247, blue: 247)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.tableViewContacto.endUpdates()
+        
+    }
+    
+
+    
     
 }
