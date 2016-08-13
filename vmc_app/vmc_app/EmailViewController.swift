dcc2f8143368b999a9b0da57485ac1114f2ecbcf
Daniel Garcia <daniel2864@gmail.com>
2016-08-12 22:46:09 -0430

    no message

--- vmc_app/vmc_app/EmailViewController.swift
+++ vmc_app/vmc_app/EmailViewController.swift
@@ -9,18 +9,26 @@
 import UIKit
 class EmailViewController: UICollectionViewController {
     
-    var datosColeccion = ["elemnto 1","elemnto 2", "elemnto 3","elemnto 4","elemnto 5"]
+    var tituloColeccion = ["elemnto 1","elemnto 2", "elemnto 3","elemnto 4","elemnto 5"]
+    var imgTituloColeccion = ["elemnto 1","elemnto 2", "elemnto 3","elemnto 4","elemnto 5"]
+    var thumbColeccion = ["elemnto 1","elemnto 2", "elemnto 3","elemnto 4","elemnto 5"]
+    var footerColeccion = ["elemnto 1","elemnto 2", "elemnto 3","elemnto 4","elemnto 5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.datosColeccion.count
+        return self.thumbColeccion.count
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell_colletion", forIndexPath: indexPath) as UICollectionViewCell
+        let thumbnail = cell.viewWithTag(2) as! UIImageView
+        thumbnail.image = UIImage(named: "photo_perfil.png")
+        let footer = cell.viewWithTag(3) as! UILabel
+        print(footer)
+        footer.text = self.footerColeccion[indexPath.row]
         
         return cell
     }
