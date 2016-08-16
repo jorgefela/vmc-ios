91c1557fe56af77511a1af9f26bc32a3507931e7
Daniel Garcia <daniel2864@gmail.com>
2016-08-16 12:13:06 -0430

    ajuste - detalle email

---
+++ vmc_app/vmc_app/DetalleEmailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetalleEmailViewController.swift
+//  vmc_app
+//
+//  Created by macbook3 on 16/8/16.
+//  Copyright © 2016 HispanoSoluciones. All rights reserved.
+//
+
+import UIKit
+
+class DetalleEmailViewController: UIViewController {
+    
+    @IBOutlet weak var thumbEmailDetalle: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
