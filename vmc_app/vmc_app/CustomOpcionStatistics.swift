a30e880a4430b01cf36267ee43abe88e5d23e693
Daniel Garcia <daniel2864@gmail.com>
2016-08-26 08:40:23 -0430

    no message

---
+++ vmc_app/vmc_app/CustomOpcionStatistics.swift
@@ -0,0 +1,13 @@
+//
+//  CustomOpcionStatistics.swift
+//  vmc_app
+//
+//  Created by macbook3 on 26/8/16.
+//  Copyright © 2016 HispanoSoluciones. All rights reserved.
+//
+
+import UIKit
+
+class CustomOpcionStatistics: UITableViewCell {
+
+}
